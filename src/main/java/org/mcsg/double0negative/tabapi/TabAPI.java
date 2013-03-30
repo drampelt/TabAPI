7615061468847ecb5a557490ded2d11086e19a7c
Daniel Rampelt <drampelt@gmail.com>
2013-03-30 12:11:47 -0400

    Converted to maven

--- src/main/java/org/mcsg/double0negative/tabapi/TabAPI.java
+++ src/main/java/org/mcsg/double0negative/tabapi/TabAPI.java
@@ -3,18 +3,12 @@ package org.mcsg.double0negative.tabapi;
 import java.io.IOException;
 import java.lang.reflect.InvocationTargetException;
 import java.util.HashMap;
-import java.util.HashSet;
-
-
-import net.minecraft.server.v1_4_R1.Packet201PlayerInfo;
 
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
-import org.bukkit.block.Block;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
-import org.bukkit.craftbukkit.v1_4_R1.entity.CraftPlayer;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
@@ -323,7 +317,7 @@ public class TabAPI extends JavaPlugin implements Listener, CommandExecutor{
 		for(int a = 0; a < r; a++){
 			s = " "+s;
 		}
-		s = s + "§" + colors[e];
+		s = s + "ï¿½" + colors[e];
 		e++; 
 		if(e > 14){
 			e = 0;
